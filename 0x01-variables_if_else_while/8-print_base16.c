5d239c0a0fbfc115d859099b129345d05f78afcf
shib07a <mevansmag@gmail.com>
2023-06-21 13:45:31 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,11 +9,11 @@ int main(void)
 	int n;
 	int m;
 
-	for (n - 48; n <= 57; n++)
+	for (n = 48; n <= 57; n++)
 	{
 		putchar(n);
 	}
-	for (m - 97; m <= 102; m++)
+	for (m = 97; m <= 102; m++)
 	{
 		putchar(m);
 	}
